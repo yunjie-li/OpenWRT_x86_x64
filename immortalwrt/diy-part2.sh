225135c333b9d241262c96b50ca481d9a7e45877
yunjie <yunjie-li@users.noreply.github.com>
2025-07-12 18:32:03 +0800

    Update diy-part2.sh

--- immortalwrt/diy-part2.sh
+++ immortalwrt/diy-part2.sh
@@ -3,3 +3,5 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.253/g' package/base-files/files/bin/config_generate
 sed -i "s/ImmortalWrt/OpenWrt/g" package/base-files/files/bin/config_generate
+
+sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
